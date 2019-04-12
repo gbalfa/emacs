fbee0f3258d326cd9f6a184c32260185526604fc
Gabriel Badilla <gabriel.badilla.alfaro@gmail.com>
2019-04-12 02:23:32 -0400

    Se añade soporte para Python.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,5 +1,14 @@
 def Settings( **kwargs ):
-  return {
-    'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', 
-'-fretain-comments-from-system-headers' ]
-  }
+  language = kwargs[ 'language' ]
+  if language == 'cfamily':
+    return {
+      # Settings for the C-family completer.
+      'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', 
+                 '-fretain-comments-from-system-headers' ]
+    }
+  if language == 'python':
+    return {
+      # Settings for the Python completer.
+      'interpreter_path': '/usr/bin/python'
+    }
+  return {}
